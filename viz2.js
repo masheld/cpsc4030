0ac5538b647da1c2376db062f7487acdefdff209
mftater <143367464+mftater@users.noreply.github.com>
2023-12-03 20:05:10 -0500

    Add files via upload

--- viz2.js
+++ viz2.js
@@ -7,38 +7,23 @@ const width = svgWidth - margin.left - margin.right;
 const height = svgHeight - margin.top - margin.bottom;
 
 
-const svg = d3.select("#stackedBar")
-    .append("svg")
-    .style("width", svgWidth)
-    .style("height", svgHeight)
+const svg = d3.create("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
 
-d3.csv("condensedGenresFixed.csv").then(function(data){
+d3.csv("/condensedGenresFixed.csv").then(function(data){
 
-    let acoustic = data.map(function (d) {
-        return +d['AcousticnessPercent']; // Use + to convert the string to a number
-    })
-    let dance = data.map(function (d) {
-        return +d['DanceabilityPercent'];
-    })
-    let valence = data.map(function (d) {
-        return +d['ValencePercent'];
-    })
-    let speech = data.map(function (d) {
-        return +d['SpeechinessPercent'];
-    })
-    let live = data.map(function (d) {
-        return +d['LivenessPercent'];
-    })
-    let energy = data.map(function (d) {
-        return +d['EnergyPercent'];
-    })
-    let years = data.map(function (d) {
-        return +d['Year']; // +converts to numerical value
-    })
+    let acoustic = data.map(d => +d['AcousticnessPercent']);
+    let dance = data.map(d => +d['DanceabilityPercent']);
+    let valence = data.map(d => +d['ValencePercent']);
+    let speech = data.map(d => +d['SpeechinessPercent']);
+    let live = data.map(d => +d['LivenessPercent']);
+    let energy = data.map(d => +d['EnergyPercent']);
 
+    var years = data.map(d => +d['Year']);
     const x = d3.scaleBand()
         .domain(years.map(String)) // Map years to strings for categorical scale
         .range([0, width])
@@ -50,29 +35,27 @@ d3.csv("condensedGenresFixed.csv").then(function(data){
 
     const bars = svg.selectAll(".bar")
         .data(data)
-        .enter().append("g")
+        .join("g")
         .attr("class", "bar")
-        .attr("transform", d => "translate(" + x(String(d['Year'])) + ",0)");
+        .attr("transform", d => `translate(${x(String(d['Year']))},0)`);
 
     bars.selectAll("rect")
-        .data(function (d) {
-            return [ // Use an array to provide a data array for each bar
-                { category: 'Acoustic', value: +d['AcousticnessPercent'] },
-                { category: 'Dance', value: +d['DanceabilityPercent'] },
-                { category: 'Valence', value: +d['ValencePercent'] },
-                { category: 'Speech', value: +d['SpeechinessPercent'] },
-                { category: 'Live', value: +d['LivenessPercent'] },
-                { category: 'Energy', value: +d['EnergyPercent'] }
-            ];
-        })
-        .enter().append("rect")
+        .data(d => [
+            { category: 'Acoustic', value: +d['AcousticnessPercent'] },
+            { category: 'Dance', value: +d['DanceabilityPercent'] },
+            { category: 'Valence', value: +d['ValencePercent'] },
+            { category: 'Speech', value: +d['SpeechinessPercent'] },
+            { category: 'Live', value: +d['LivenessPercent'] },
+            { category: 'Energy', value: +d['EnergyPercent'] }
+        ])
+        .join("rect")
         .attr("height", d => height - y(d.value))
         .attr("y", d => y(d.value))
         .attr("width", x.bandwidth())
         .style("fill", d => colorScale(d.category));
 
     svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
+        .attr("transform", `translate(0,${height})`)
         .call(d3.axisBottom(x));
 
     svg.append("g")
