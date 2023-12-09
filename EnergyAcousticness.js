63836e16f5437241c6119ef8a04a87b9226b13d8
masheld <masheld@clemson.edu>
2023-12-09 16:20:09 -0500

    increased x-axis accuracy of expanded axis, edited csv years to remove d/m info

--- EnergyAcousticness.js
+++ EnergyAcousticness.js
@@ -3,7 +3,7 @@ d3.csv("condensedGenresFixed.csv").then(
 
         var dimensions = {
             width: 1350,
-            height: 350,
+            height: 300,
             margin:{
                 top: 10,
                 bottom: 50,
@@ -68,6 +68,7 @@ d3.csv("condensedGenresFixed.csv").then(
         // Dots are assigned a class alligning to their graph and song title
         // the ____Graph class is assigned to all elements belonging to a single scatterplot
         var acousticDots = svg.append("g")
+                      .attr('id', "acoustic")
                       .selectAll("circle")
                       .data(dataset)
                       .enter()
@@ -77,7 +78,10 @@ d3.csv("condensedGenresFixed.csv").then(
                       .attr("r", radius)
                       .attr("fill", "steelblue")
                       .attr("fill-opacity", opacity)
-                      .attr("class", d => "acousticGraph acousticDots " + d.Title.replace(/\s+/g, '-')); // Replace spaces with dashes for valid class names 
+                      .attr("class", d => "acousticGraph acousticDots " + 
+                            d.Title.replace(/\s+/g, '-').replace(/[,.'\/&\(\)]+/g, '') + 
+                            " " + d.TopGenre.replace(/\s+/g, '-').replace(/[\/&]+/g, '') + 
+                            " " + d.Year) // Replace or remove non alphanumeric characters 
 
         var danceDots = svg.append("g")
                       .selectAll("circle")
@@ -89,7 +93,9 @@ d3.csv("condensedGenresFixed.csv").then(
                       .attr("r", radius)
                       .attr("fill", "orange")
                       .attr("fill-opacity", opacity)
-                      .attr("class", d => "danceGraph danceDots " + d.Title.replace(/\s+/g, '-'))
+                      .attr("class", d => "danceGraph danceDots " + 
+                            d.Title.replace(/\s+/g, '-').replace(/[,.'\/&\(\)]+/g, '') + 
+                            " " + d.TopGenre.replace(/\s+/g, '-').replace(/[\/&]+/g, '')) // Replace or remove non alphanumeric characters 
 
         var valenceDots = svg.append("g")
                       .selectAll("circle")
@@ -101,7 +107,9 @@ d3.csv("condensedGenresFixed.csv").then(
                       .attr("r", radius)
                       .attr("fill", "green")
                       .attr("fill-opacity", opacity)
-                      .attr("class", d => "valenceGraph valenceDots " + d.Title.replace(/\s+/g, '-'))
+                      .attr("class", d => "valenceGraph valenceDots " + 
+                            d.Title.replace(/\s+/g, '-').replace(/[,.'\/&\(\)]+/g, '') + 
+                            " " + d.TopGenre.replace(/\s+/g, '-').replace(/[\/&]+/g, '')) // Replace or remove non alphanumeric characters 
 
         var speechDots = svg.append("g")
                       .selectAll("circle")
@@ -113,7 +121,9 @@ d3.csv("condensedGenresFixed.csv").then(
                       .attr("r", radius)
                       .attr("fill", "red")
                       .attr("fill-opacity", opacity)
-                      .attr("class", d => "speechGraph speechDots " + d.Title.replace(/\s+/g, '-'))
+                      .attr("class", d => "speechGraph speechDots " + 
+                            d.Title.replace(/\s+/g, '-').replace(/[,.'\/&\(\)]+/g, '') + 
+                            " " + d.TopGenre.replace(/\s+/g, '-').replace(/[\/&]+/g, '')) // Replace or remove non alphanumeric characters 
 
         var liveDots = svg.append("g")
                       .selectAll("circle")
@@ -125,8 +135,10 @@ d3.csv("condensedGenresFixed.csv").then(
                       .attr("r", radius)
                       .attr("fill", "purple")
                       .attr("fill-opacity", opacity)
-                      .attr("class", d => "liveGraph liveDots " + d.Title.replace(/\s+/g, '-'))
-
+                      .attr("class", d => "liveGraph liveDots " + 
+                            d.Title.replace(/\s+/g, '-').replace(/[,.'\/&\(\)]+/g, '') + 
+                            " " + d.TopGenre.replace(/\s+/g, '-').replace(/[\/&]+/g, '')) // Replace or remove non alphanumeric characters 
+                    
 
         // Generate axes 
         var xAxisGen = d3.axisBottom().scale(acousticScale)
@@ -329,33 +341,117 @@ d3.csv("condensedGenresFixed.csv").then(
         function expandAxis(clickedLabel) {
             if(clickedLabel.classed("acousticGraph")) {
                 acousticScale.range([dimensions.margin.left, dimensions.width-dimensions.margin.right])
+                xAxisGen.ticks(20);
                 d3.select("#acousticGraph").call(xAxisGen)
-                                            .transition()
-                                            .duration(1500);
+
+                // ***************************************
+                // Add additional ticks
+                const minorTickValues = d3.range(0, 1.01, 0.01).filter(d => d % 0.1 !== 0); // Values for minor ticks, excluding major tick values
+
+                d3.select("#acousticGraph").selectAll(".minor-tick")
+                .data(minorTickValues)
+                .enter().append("line")
+                    .attr("class", "minor-tick")
+                    .attr("y1", 0)
+                    .attr("y2", 3)
+                    .attr("x1", d => acousticScale(d))
+                    .attr("x2", d => acousticScale(d)+1)
+                    .style("stroke", "black") // Style as needed
+                    .style("stroke-width", 1);
+                    
+                d3.selectAll(".minor-tick").style("display", null);
+                // ***************************************
                 d3.selectAll(".acousticDots").attr("cx", d => acousticScale(acousticAccessor(d)))
                 d3.select("#acousticLabel").attr("x", dimensions.width/2)
             }
             else if(clickedLabel.classed("danceGraph")) {
                 danceScale.range([dimensions.margin.left, dimensions.width-dimensions.margin.right])
+                danceAxisGen.ticks(20)
                 d3.select("#danceGraph").call(danceAxisGen)
+
+                // Add additional ticks
+                const minorTickValues = d3.range(0, 1.01, 0.01).filter(d => d % 0.1 !== 0); // Values for minor ticks, excluding major tick values
+
+                d3.select("#danceGraph").selectAll(".minor-tick")
+                .data(minorTickValues)
+                .enter().append("line")
+                    .attr("class", "minor-tick")
+                    .attr("y1", 0)
+                    .attr("y2", 3)
+                    .attr("x1", d => danceScale(d))
+                    .attr("x2", d => danceScale(d)+1)
+                    .style("stroke", "black") // Style as needed
+                    .style("stroke-width", 1);
+                    
+                d3.selectAll(".minor-tick").style("display", null);
                 d3.selectAll(".danceDots").attr("cx", d => danceScale(danceAccessor(d))) 
                 d3.select("#danceLabel").attr("x", dimensions.width/2)
             }
             else if(clickedLabel.classed("valenceGraph")) {
                 valenceScale.range([dimensions.margin.left, dimensions.width-dimensions.margin.right])
+                valenceAxisGen.ticks(20)
                 d3.select("#valenceGraph").call(valenceAxisGen)
+                // Add additional ticks
+                const minorTickValues = d3.range(0, 1.01, 0.01).filter(d => d % 0.1 !== 0); // Values for minor ticks, excluding major tick values
+
+                d3.select("#valenceGraph").selectAll(".minor-tick")
+                .data(minorTickValues)
+                .enter().append("line")
+                    .attr("class", "minor-tick")
+                    .attr("y1", 0)
+                    .attr("y2", 3)
+                    .attr("x1", d => valenceScale(d))
+                    .attr("x2", d => valenceScale(d)+1)
+                    .style("stroke", "black") // Style as needed
+                    .style("stroke-width", 1);
+                    
+                d3.selectAll(".minor-tick").style("display", null);
                 d3.selectAll(".valenceDots").attr("cx", d => valenceScale(valenceAccessor(d))) 
                 d3.select("#valenceLabel").attr("x", dimensions.width/2)
             }
             else if(clickedLabel.classed("speechGraph")) {
                 speechScale.range([dimensions.margin.left, dimensions.width-dimensions.margin.right])
+                speechAxisGen.ticks(20)
                 d3.select("#speechGraph").call(speechAxisGen)
+
+                // Add additional ticks
+                const minorTickValues = d3.range(0, 1.01, 0.01).filter(d => d % 0.1 !== 0); // Values for minor ticks, excluding major tick values
+
+                d3.select("#speechGraph").selectAll(".minor-tick")
+                .data(minorTickValues)
+                .enter().append("line")
+                    .attr("class", "minor-tick")
+                    .attr("y1", 0)
+                    .attr("y2", 3)
+                    .attr("x1", d => speechScale(d))
+                    .attr("x2", d => speechScale(d)+1)
+                    .style("stroke", "black") // Style as needed
+                    .style("stroke-width", 1);
+                    
+                d3.selectAll(".minor-tick").style("display", null);
                 d3.selectAll(".speechDots").attr("cx", d => speechScale(speechAccessor(d))) 
                 d3.select("#speechLabel").attr("x", dimensions.width/2)
             }
             else if(clickedLabel.classed("liveGraph")) {
                 liveScale.range([dimensions.margin.left, dimensions.width-dimensions.margin.right])
+                liveAxisGen.ticks(20)
                 d3.select("#liveGraph").call(liveAxisGen)
+
+                // Add additional ticks
+                const minorTickValues = d3.range(0, 1.01, 0.01).filter(d => d % 0.1 !== 0); // Values for minor ticks, excluding major tick values
+
+                d3.select("#liveGraph").selectAll(".minor-tick")
+                .data(minorTickValues)
+                .enter().append("line")
+                    .attr("class", "minor-tick")
+                    .attr("y1", 0)
+                    .attr("y2", 3)
+                    .attr("x1", d => liveScale(d))
+                    .attr("x2", d => liveScale(d)+1)
+                    .style("stroke", "black") // Style as needed
+                    .style("stroke-width", 1);
+                    
+                d3.selectAll(".minor-tick").style("display", null);
                 d3.selectAll(".liveDots").attr("cx", d => liveScale(liveAccessor(d))) 
                 d3.select("#liveLabel").attr("x", dimensions.width/2)
             }
@@ -365,33 +461,43 @@ d3.csv("condensedGenresFixed.csv").then(
         function collapseAxis(clickedLabel) {
             if(clickedLabel.classed("acousticGraph")) {
                 acousticScale.range([dimensions.margin.left, calcAxisWidth(1, 2)])
+                xAxisGen.ticks(10);
                 d3.select("#acousticGraph").call(xAxisGen)
                                             .transition()
                                             .duration(1500);
+                d3.selectAll(".minor-tick").style("display", "none")
                 d3.selectAll(".acousticDots").attr("cx", d => acousticScale(acousticAccessor(d))) 
                 d3.select("#acousticLabel").attr("x", calcLabelX(1, 2))
             }
             else if(clickedLabel.classed("danceGraph")) {
                 danceScale.range([calcAxisWidth(1, 1), calcAxisWidth(2, 2)])
+                danceAxisGen.ticks(10);
                 d3.select("#danceGraph").call(danceAxisGen)
+                d3.selectAll(".minor-tick").style("display", "none")
                 d3.selectAll(".danceDots").attr("cx", d => danceScale(danceAccessor(d))) 
                 d3.select("#danceLabel").attr("x", calcLabelX(2, 2))
             }
             else if(clickedLabel.classed("valenceGraph")) {
                 valenceScale.range([calcAxisWidth(2, 1), calcAxisWidth(3, 2)])
+                valenceAxisGen.ticks(10)
                 d3.select("#valenceGraph").call(valenceAxisGen)
+                d3.selectAll(".minor-tick").style("display", "none")
                 d3.selectAll(".valenceDots").attr("cx", d => valenceScale(valenceAccessor(d))) 
                 d3.select("#valenceLabel").attr("x", calcLabelX(3, 2))
             }
             else if(clickedLabel.classed("speechGraph")) {
                 speechScale.range([calcAxisWidth(3, 1), calcAxisWidth(4, 2)])
+                speechAxisGen.ticks(10)
                 d3.select("#speechGraph").call(speechAxisGen)
+                d3.selectAll(".minor-tick").style("display", "none")
                 d3.selectAll(".speechDots").attr("cx", d => speechScale(speechAccessor(d))) 
                 d3.select("#speechLabel").attr("x", calcLabelX(4, 2))
             }
             else if(clickedLabel.classed("liveGraph")) {
                 liveScale.range([calcAxisWidth(4, 1), calcAxisWidth(5, 2)])
+                liveAxisGen.ticks(10)
                 d3.select("#liveGraph").call(liveAxisGen)
+                d3.selectAll(".minor-tick").style("display", "none")
                 d3.selectAll(".liveDots").attr("cx", d => liveScale(liveAccessor(d))) 
                 d3.select("#liveLabel").attr("x", calcLabelX(5, 2))
             }
@@ -415,6 +521,6 @@ d3.csv("condensedGenresFixed.csv").then(
                 unhideElements();
                 collapseAxis(clickedLabel);
             }
-        });
+        })
     }
 )
